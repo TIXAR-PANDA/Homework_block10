a9385a6234582735676ce0a07a6fb49563a02084
TIXAR-PANDA <natali040168@mail.ru>
2024-11-30 12:59:30 +0300

    Add files via upload

--- module_10_3.py
+++ module_10_3.py
@@ -5,6 +5,7 @@ import threading
 class Bank:
     """
     balance - баланс банка (int)
+    lock - объект класса Lock для блокировки потоков.
     """
 
     def __init__(self):
@@ -12,25 +13,39 @@ class Bank:
         self.lock = threading.Lock()
 
     def deposit(self):
-        for i in range(100):
+        for i in range(100): # Будет совершать 100 транзакций пополнения средств.
+            addition = random.randint(50, 500) # пополнение - случайное целое число от 50 до 500.
+   # если баланс больше или равен 500 и замок lock заблокирован - lock.locked(),
             if self.balance >= 500 and self.lock.locked():
+                # то разблокировать его методом release.
                 self.lock.release()
-            addition = random.randint(50, 500)
-            self.balance += addition
+            self.balance += addition  # пополнили баланс
+
+    # После увеличения баланса должна выводится строка
+            # "Пополнение: <случайное число>. Баланс: <текущий баланс>".
             print(f'Пополнение: {addition}. Баланс: {self.balance}')
             time.sleep(0.001)
 
     def take(self):
-        for i in range(100):
-            removing = random.randint(50, 500)
+        for i in range(100): # Будет совершать 100 транзакций снятия.
+            removing = random.randint(50, 500) # Снятие - случайное целое число от 50 до 500.
+    # В начале должно выводится сообщение "Запрос на <случайное число>".
             print(f'Запрос на {removing}')
+    # Далее производится проверка: если случайное число меньше или равно текущему балансу,
             if self.balance >= removing:
+    # то произвести снятие, уменьшив balance
                 self.balance -= removing
+
+    # и вывести на экран "Снятие: <случайное число>. Баланс: <текущий баланс>".
                 print(f'Снятие: {removing}. Баланс: {self.balance}')
-            else:
+                time.sleep(0.001)
+            else: # Если случайное число оказалось больше баланса,
+                # то вывести строку "Запрос отклонён, недостаточно средств"
                 print(f'Запрос отклонён, недостаточно средств')
+                time.sleep(0.001)
+                # и заблокировать поток методом acquire.
                 self.lock.acquire()
-            time.sleep(0.001)
+
 
 
 bk = Bank()
